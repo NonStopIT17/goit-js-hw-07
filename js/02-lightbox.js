e1a110a2b8cf9a3f25c06c7e4b71424a1abac2ba
Katerina <118116963+NonStopIT17@users.noreply.github.com>
2023-04-02 14:22:52 +0300

    goit-js-hw-07
    Finish

---
+++ js/02-lightbox.js
@@ -0,0 +1,22 @@
+import { galleryItems } from "./gallery-items.js";
+console.log(galleryItems);//гелерея
+const gallery = document.querySelector(".gallery");
+const imageMarkup = creatSrtElmGallImg(galleryItems);
+gallery.insertAdjacentHTML("beforeend", imageMarkup);
+function creatSrtElmGallImg(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" onclick="event.preventDefault()"
+     href="${original}">
+  <img class="gallery__image"
+   src="${preview}"
+    alt="${description}" />
+</a>`;
+    })
+    .join("");//зєднуємо
+}
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  overlayOpacity: 0.7,
+});
